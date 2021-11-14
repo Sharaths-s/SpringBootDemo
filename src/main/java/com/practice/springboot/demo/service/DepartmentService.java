0d517446a54851549c3363110b363a40f1094386
Sharaths-s <68059016+Sharaths-s@users.noreply.github.com>
2021-11-14 20:50:19 +0530

    Added all the features of delete, update and also get by ID and name

--- src/main/java/com/practice/springboot/demo/service/DepartmentService.java
+++ src/main/java/com/practice/springboot/demo/service/DepartmentService.java
@@ -2,6 +2,18 @@ package com.practice.springboot.demo.service;
 
 import com.practice.springboot.demo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     Department saveDepartment(Department department);
+
+    List<Department> fetchDepartmentList();
+
+    Department fetchDepartmentById(Long departmentId);
+
+    void deleteDepartmentById(Long departmentId);
+
+    Department updateDepartment(Long departmentId, Department department);
+
+    Department fetchDepartmentByName(String departmentName);
 }
